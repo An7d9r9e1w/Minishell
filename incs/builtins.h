5618fd94212775c6174804b3e97fdf9f2c648090
Noodles Namor <nnamor@er-e1.kzn.21-school.ru>
2021-08-04 13:28:18 +0300

    Rename main.h to builtins.h
    "main.h" doesn't describe content of header.

--- incs/builtins.h
+++ incs/builtins.h
@@ -1,20 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.h                                             :+:      :+:    :+:   */
+/*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ejina <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 11:49:04 by ejina             #+#    #+#             */
-/*   Updated: 2021/08/04 11:50:46 by nnamor           ###   ########.fr       */
+/*   Updated: 2021/08/04 13:20:23 by nnamor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MAIN_H
-# define MAIN_H
+#ifndef BUILTINS_H
+# define BUILTINS_H
 
 # include <vvector.h>
 
+t_vvector	*get_env(char **env);
 int			msh_unset(char **args, t_vvector *env);
 int			msh_env(char **args, t_vvector *env);
 int			msh_pwd(char **args);
@@ -22,8 +23,7 @@ int			msh_echo(char **args);
 int			msh_cd(char **args, t_vvector *env);
 int			msh_export(char **args, t_vvector *env);
 int			msh_exit(char **args);
-int			ft_strcmp(const void *s1, const void *s2);
-int			ft_strlen(const char *str);
-t_vvector	*get_env(char **env);
+/*int			ft_strcmp(const void *s1, const void *s2);*/
+/*int			ft_strlen(const char *str);*/
 
-#endif	/*MAIN_H*/
+#endif	/*BUILTINS_H*/
