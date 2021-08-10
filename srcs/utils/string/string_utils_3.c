f39a510fb9c38c922be245467ddc0519e7d53619
Noodles Namor <nnamor@er-e1.kzn.21-school.ru>
2021-08-10 14:20:02 +0300

    Add string function - matoi
    matoi converts string to integer

--- srcs/utils/string/string_utils_3.c
+++ srcs/utils/string/string_utils_3.c
@@ -6,13 +6,14 @@
 /*   By: ejina <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 11:58:08 by ejina             #+#    #+#             */
-/*   Updated: 2021/08/04 11:58:27 by nnamor           ###   ########.fr       */
+/*   Updated: 2021/08/10 14:19:19 by nnamor           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <error.h>
 #include <string_utils.h>
+#include <char_checkers.h>
 
 char	*mstrjoin(char const *s1, char const *s2)
 {
@@ -39,4 +40,24 @@ char	*mstrjoin(char const *s1, char const *s2)
 	}
 	p[i] = 0;
 	return (p);
+}
+
+char	*matoi(char *s1)
+{
+	int	num;
+	int	sign;
+
+	if (!s1)
+		return (0);
+	while (is_space(*s1))
+		++s1;
+	sign = 0;
+	while (*s1 == '-' || *s1 == '+')
+		sign ^= (*s1 == '-');
+	num = 0;
+	while (*s1 >= '0' && *s1 <= '9')
+		num = num * 10 + *s1++ - '0';
+	if (sign)
+		num = -num;
+	return (num);
 }
